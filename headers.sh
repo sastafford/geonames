ee1a4d77851825963e63dd65ce9ced25b3be6014
Scott Stafford <scott.stafford@marklogic.com>
2014-09-10 19:17:29 -0700

    remove all files

--- headers.sh
+++
@@ -1,3 +0,0 @@
-COUNTRY=$1
-
-sed '1 i\geonameid	name	asciiname	alternatenames	latitude	longitude	feature-class	feature-code	country-code	cc2	admin1-code	admin2-code	admin3-code	admin4-code	population	elevation	dem	timezone	modification-date' data/$COUNTRY/$COUNTRY.txt > temp.txt && mv temp.txt data/$COUNTRY/$COUNTRY.txt 
